6c6797b454fe3a3be63ddf11308bc4dc24a1a54a
Mac <mac@Macs-MacBook-Pro.local>
2025-05-14 20:02:59 +0100

    prod url test

--- src/services/Config.ts
+++ src/services/Config.ts
@@ -1,5 +1,5 @@
 let Config = {
-  apiUrl: process.env.NODE_ENV === 'development' ? 'https://dev.api.cloudman.site/v1/api/' : '',
+  apiUrl: process.env.NODE_ENV === 'development' ? 'https://dev.api.cloudman.site/v1/api/' : 'https://dev.api.cloudman.site/v1/api/',
   devMode: process.env.NODE_ENV,
 };
 
