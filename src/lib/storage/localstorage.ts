6ec6abe371050f65f3992eb3ff58c3830aebeaa1
Mac <mac@Macs-MacBook-Pro.local>
2025-06-15 13:15:36 +0100

    fix

--- src/lib/storage/localstorage.ts
+++ src/lib/storage/localstorage.ts
@@ -2,16 +2,16 @@ import * as localStorage from "local-storage";
 
 export const storage = {
   async save(key: string, value: string) {
-    // localStorage.set(key, value);
-    sessionStorage.setItem("key", "value");
+    localStorage.set(key, value);
+    // sessionStorage.setItem('key', 'value');
   },
   async getValueFor(key: string) {
-    // return localStorage.get(key);
-    return sessionStorage.getItem(key);
+    return localStorage.get(key);
+    // return sessionStorage.getItem(key);
   },
   async remove(key: string) {
-    // localStorage.clear()
-    sessionStorage.clear();
+    localStorage.clear();
+    // sessionStorage.clear();
   },
   async clearAuth() {
     sessionStorage.clear();
